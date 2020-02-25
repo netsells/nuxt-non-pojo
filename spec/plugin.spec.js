5226ec522dedaeb24504d7433b58bbebe7967a36
Sam Boylett <sam.boylett@netsells.co.uk>
2020-02-25 16:02:10 +0000

    tests

--- spec/plugin.spec.js
+++ spec/plugin.spec.js
@@ -8,8 +8,8 @@ const TestBase = {
 };
 
 class Base {
-    constructor() {
-        this.fields = {};
+    constructor(fields = {}) {
+        this.fields = fields;
     }
 
     toPOJO() {
@@ -22,13 +22,14 @@ class Base {
 
     static fromPOJO(fields) {
         const Klass = this;
-        const k = new Klass();
-        k.fields = fields;
+
+        return new Klass(fields);
     }
 }
 
 class Foo extends Base {}
 class Bar extends Base {}
+class Broken {}
 
 describe('plugin', () => {
     it('exports a Vue plugin', () => {
@@ -46,8 +47,12 @@ describe('plugin', () => {
             localVue = createLocalVue();
             localVue.use(Vuex);
 
-            app = {};
             store = new Vuex.Store();
+            app = new class {
+                constructor() {
+                    this.store = store;
+                }
+            };
 
             localVue.use(plugin, {
                 store,
@@ -74,6 +79,59 @@ describe('plugin', () => {
 
             it('adds the $nnp mixin', () => {
                 expect(wrapper.vm.$nnp).toEqual(expect.any(Function));
+            });
+        });
+
+        describe('$nnp', () => {
+            it('will return null if not found', () => {
+                expect(app.$nnp('foo')).toBe(undefined);
+            });
+
+            describe('save', () => {
+                it('will throw if class not registered', () => {
+                    expect(() => {
+                        app.$nnp.save(new Broken())
+                    }).toThrow(new Error('Passed values class has not been registered: "Broken"'));
+                });
+
+                it('will throw if not an object', () => {
+                    expect(() => {
+                        app.$nnp.save(5)
+                    }).toThrow(new Error('Passed value is not a class instance'));
+                });
+
+                it('returns the identity', () => {
+                    expect(app.$nnp.save(new Foo({ id: 5 }))).toEqual({
+                        key: 5,
+                        name: 'Foo',
+                    });
+                });
+
+                describe('when objects saved', () => {
+                    let objects;
+                    let pojos;
+
+                    beforeEach(() => {
+                        objects = {};
+                        pojos = {};
+
+                        objects.foo1 = new Foo({ id: 1, foo: 'foo' });
+                        objects.foo2 = new Foo({ id: 2, foo: 'bar' });
+                        objects.bar1 = new Bar({ id: 1, bar: 'foo' });
+                        objects.bar2 = new Bar({ id: 2, bar: 'bar' });
+
+                        Object.keys(objects).forEach(k => {
+                            pojos[k] = app.$nnp.save(objects[k]);
+                        });
+                    });
+
+                    it('will load the correct ones', () => {
+                        expect(app.$nnp(pojos.foo1)).toEqual(objects.foo1);
+                        expect(app.$nnp(pojos.foo2)).toEqual(objects.foo2);
+                        expect(app.$nnp(pojos.bar1)).toEqual(objects.bar1);
+                        expect(app.$nnp(pojos.bar2)).toEqual(objects.bar2);
+                    });
+                });
             });
         });
     });
