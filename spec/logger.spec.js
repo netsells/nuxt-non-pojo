4f15b778aa73876f4262387c96c30c5105fc7f8f
Sam Boylett <sam.boylett@netsells.co.uk>
2020-02-26 15:18:06 +0000

    Kill all mutants

--- spec/logger.spec.js
+++ spec/logger.spec.js
@@ -1,7 +1,13 @@
+import consola from 'consola';
+
 import logger from '../src/logger';
 
 describe('logger', () => {
     it('exists', () => {
         expect(logger).toBeTruthy();
+    });
+
+    it('creates a scoped consola', () => {
+        expect(logger._defaults).toEqual(consola.withScope('netsells:nuxt-non-pojo')._defaults);
     });
 });
